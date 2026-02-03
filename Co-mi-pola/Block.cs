008b9a56389db9b8a07fc5ae9d50cbc93bcd1be3
Fluff <112392081+Fluff74@users.noreply.github.com>
2026-02-03 13:58:52 -0500

    Added blocks to the game, and created the floor.

---
+++ Co-mi-pola/Block.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Co_mi_pola
+{
+    // Joshua Smith
+    // 02/03/2026
+    //
+    // A block is just a rectangle that can be stepped on or walked into. This can be a platform, a ledge, or even just the ground.
+    internal class Block(Rectangle hitbox, Texture2D texture)
+    {
+        /// <summary>
+        /// The hitbox of the Block object.
+        /// </summary>
+        public Rectangle Hitbox { get; set; } = hitbox;
+
+        /// <summary>
+        /// Draws the block to the screen.
+        /// </summary>
+        /// <param name="sb"> The SpriteBatch being used to draw the block. </param>
+        /// <param name="color"> The color of the block. </param>
+        public void Draw(SpriteBatch sb, Color color)
+        {
+            sb.Draw(texture, Hitbox, color);
+        }
+    }
+}
--- Co-mi-pola/Game1.cs
+++ Co-mi-pola/Game1.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
 
 namespace Co_mi_pola
 {
@@ -29,11 +29,17 @@ namespace Co_mi_pola
         }
         GameState gameState;
 
+        Texture2D blockTexture; // A 1x1 pixel used to render all blocks.
+
+        #region Colors
         Color foregroundColor; // The color of the player, ground, and everything other than the background.
         Color backgroundColor; // The color of the background.
         Color[] foregroundColors; // All possible colors that can be in the foreground.
         Color[] backgroundColors; // All possible colors that can be in the background.
         bool swapped; // Whether or not we've swapped the color palettes. Could be a cute easter egg.
+        #endregion
+
+        Block floor; // The floor used on most, if not all scenes.
 
         public Game1()
         {
@@ -60,11 +66,13 @@ namespace Co_mi_pola
 
             gameState = GameState.MainMenu; // Start the player off on the main menu.
 
+            #region Colors
             // Initialize the color palettes, and all color related variables.
-            foregroundColors = [new(255, 94, 94), new(255, 186, 74), new(237, 225, 90), new(134, 224, 123), new(49, 228, 235), new(62, 83, 222), new(177, 41, 204), new(222, 78, 198), new(214, 214, 214)];
-            backgroundColors = [new(64, 8, 0),    new(79, 61, 0),    new(79, 78, 0),    new(19, 69, 12),    new(0, 64, 66),    new(0, 9, 69),    new(66, 0, 79),    new(74, 0, 41),    new(26, 26, 26)];
+            foregroundColors = [new(64, 8, 0), new(79, 61, 0), new(79, 78, 0), new(19, 69, 12), new(0, 64, 66), new(0, 9, 69), new(66, 0, 79), new(74, 0, 41), new(26, 26, 26)];
+            backgroundColors = [new(255, 94, 94), new(255, 186, 74), new(237, 225, 90), new(134, 224, 123), new(49, 228, 235), new(62, 83, 222), new(177, 41, 204), new(222, 78, 198), new(214, 214, 214)];
             swapped = false;
             RandomizePalette(swapped);
+            #endregion
 
             base.Initialize();
         }
@@ -72,6 +80,9 @@ namespace Co_mi_pola
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            blockTexture = Content.Load<Texture2D>($"Block");
+            floor = new Block(new Rectangle(0, 63, 128, 9), blockTexture);
         }
 
         protected override void Update(GameTime gameTime)
@@ -107,7 +118,7 @@ namespace Co_mi_pola
         {
             #region Begin Draw
             GraphicsDevice.SetRenderTarget(_renderTarget);
-            GraphicsDevice.Clear(Color.White);
+            GraphicsDevice.Clear(backgroundColor);
             _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
             #endregion
 
@@ -115,7 +126,7 @@ namespace Co_mi_pola
             {
                 case GameState.MainMenu:
 
-
+                    floor.Draw(_spriteBatch, foregroundColor);
 
                     break;
 
